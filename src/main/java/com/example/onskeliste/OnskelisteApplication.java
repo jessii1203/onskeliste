8067da1e2706f23c54dcd232f32fefacea377ea2
jessi1203 <jessii.ca@live.dk>
2022-10-25 09:27:19 +0200

    Initial commit

---
+++ src/main/java/com/example/onskeliste/OnskelisteApplication.java
@@ -0,0 +1,13 @@
+package com.example.onskeliste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnskelisteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnskelisteApplication.class, args);
+    }
+
+}
